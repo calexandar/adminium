a354722a86a00940878cdef1c36303b74c0829f4
calexandar <alexandercvet243@gmail.com>
2025-10-01 21:00:25 +0200

    aded alert for success

--- admin/Users/UsersController.php
+++ admin/Users/UsersController.php
@@ -63,7 +63,7 @@ final readonly class UsersController
 
         $user->syncPermissions($selectedPermissions);
 
-        return redirect()->route('admin.users.index');
+        return redirect()->route('admin.users.index')->with('success', 'User updated successfully');
     }
 
     public function destroy(string $user): RedirectResponse
@@ -72,6 +72,6 @@ final readonly class UsersController
 
         $user->delete();
 
-        return redirect()->route('admin.users.index');
+        return redirect()->route('admin.users.index')->with('success', 'User deleted successfully');
     }
 }
