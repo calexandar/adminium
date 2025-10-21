65349c93de4e5b1e4944a53d214293812d3872e7
calexandar <alexandercvet243@gmail.com>
2025-10-21 20:23:57 +0200

    chore: aded available locales

--- config/app.php
+++ config/app.php
@@ -82,6 +82,8 @@ return [
 
     'locale' => env('APP_LOCALE', 'en'),
 
+    'available_locales' => ['en', 'mk', 'al'],
+
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
