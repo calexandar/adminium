8d81ebf88a29c9214d5ed0daeff909afc9563719
calexandar <alexandercvet243@gmail.com>
2026-01-22 12:09:37 +0100

    chore: created select and textarea partials

---
+++ admin/Settings/Views/partials/textarea.blade.php
@@ -0,0 +1,9 @@
+<div class="col-span-full">
+    <label for="{{ $field['name'] }}" class="block text-sm/6 font-medium text-gray-900 dark:text-white">{{ $field['label'] }}</label>
+    <div class="mt-2">
+        <textarea id="{{ $field['name'] }}" name="{{ $field['name'] }}" rows="4" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500">{{ settings()->group($group)->get($field['name'], $field['default']) }}</textarea>
+    </div>
+    @error ($field['name'])           
+        <p id="email-error" class="mt-2 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
+    @enderror
+</div>
