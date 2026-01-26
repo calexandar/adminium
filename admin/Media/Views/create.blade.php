719afdc8117e7289422f7d682f9787a8a897fa9e
calexandar <alexandercvet243@gmail.com>
2026-01-26 19:11:42 +0100

    fix: publish checkbox

--- admin/Media/Views/create.blade.php
+++ admin/Media/Views/create.blade.php
@@ -125,10 +125,10 @@
 
             <div class="col-span-full">
               <div class="flex items-center gap-x-3">
-                <div class="media relative inline-flex w-11 shrink-0 rounded-full bg-gray-200 p-0.5 inset-ring inset-ring-gray-900/5 outline-offset-2 outline-indigo-600 transition-colors duration-200 ease-in-out has-checked:bg-indigo-600 has-focus-visible:outline-2 dark:bg-white/5 dark:inset-ring-white/10 dark:outline-indigo-500 dark:has-checked:bg-indigo-500">
-                  <span class="size-5 rounded-full bg-white shadow-xs ring-1 ring-gray-900/5 transition-transform duration-200 ease-in-out media-has-checked:translate-x-5"></span>
+                <div class="group relative inline-flex w-11 shrink-0 rounded-full bg-gray-200 p-0.5 inset-ring inset-ring-gray-900/5 outline-offset-2 outline-indigo-600 transition-colors duration-200 ease-in-out has-checked:bg-indigo-600 has-focus-visible:outline-2 dark:bg-white/5 dark:inset-ring-white/10 dark:outline-indigo-500 dark:has-checked:bg-indigo-500">
+                  <span class="size-5 rounded-full bg-white shadow-xs ring-1 ring-gray-900/5 transition-transform duration-200 ease-in-out group-has-checked:translate-x-5"></span>
                   <input type="hidden" name="published" value="0">
-                  <input id="published" type="checkbox" name="published" value="1" aria-labelledby="published-label" aria-describedby="published-description" class="absolute inset-0 appearance-none focus:outline-hidden"/>
+                  <input id="published" type="checkbox" name="published" value="1" checked aria-labelledby="published-label" aria-describedby="published-description" class="absolute inset-0 appearance-none focus:outline-hidden"/>
                 </div>
 
                 <div class="text-sm">
